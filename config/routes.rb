8b7a09810deb8e347b340ad3dad502e457ff9b11
Dexter Fitch <fitch.dexter@gmail.com>
2020-08-01 00:52:34 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   root 'posts#index'
+
+  resources :authors, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :new]
+  end
+  resources :posts
 end
